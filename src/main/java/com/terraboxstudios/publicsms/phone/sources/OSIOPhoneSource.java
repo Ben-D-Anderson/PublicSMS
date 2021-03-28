cd437fb6a669e47135c9f04ab6df1956e686e963
Ben-D-Anderson <benanderson.david@gmail.com>
2021-03-28 18:39:04 +0100

    Added getMessages method

--- src/main/java/com/terraboxstudios/publicsms/phone/sources/OSIOPhoneSource.java
+++ src/main/java/com/terraboxstudios/publicsms/phone/sources/OSIOPhoneSource.java
@@ -7,12 +7,16 @@ import com.google.gson.JsonParser;
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.terraboxstudios.publicsms.message.Message;
+import com.terraboxstudios.publicsms.phone.Phone;
 import com.terraboxstudios.publicsms.phone.PhoneSource;
 import com.terraboxstudios.publicsms.phone.PublicPhone;
 import com.terraboxstudios.publicsms.utils.HttpUtility;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashSet;
 
 public class OSIOPhoneSource implements PhoneSource {
@@ -45,7 +49,31 @@ public class OSIOPhoneSource implements PhoneSource {
 
     @Override
     public Collection<Message> getMessages(PublicPhone receivingPhone) throws IOException {
-        return null;
+        int countryCodeInt = PhoneNumberUtil.getInstance().getCountryCodeForRegion(receivingPhone.getCountryCode());
+        String countryCodeStr = "+" + countryCodeInt;
+        String shortenedNumber = receivingPhone.getNumber().replace(countryCodeStr, "");
+        String responseStr = HttpUtility.readSingleLineRespone(HttpUtility.sendGetRequest("https://onlinesim.io/api/getFreeList?lang=en&country=" + countryCodeInt + "&number=" + shortenedNumber));
+        JsonArray messagesJson = JsonParser.parseString(responseStr).getAsJsonObject().get("messages").getAsJsonObject().get("data").getAsJsonArray();
+        Collection<Message> messages = new HashSet<>();
+        for (JsonElement msgElement : messagesJson) {
+            JsonObject msgObj = msgElement.getAsJsonObject();
+            String fromStr = msgObj.get("in_number").getAsString();
+            Phone sender = null;
+            try {
+                sender = new Phone(fromStr, PhoneNumberUtil.getInstance().getRegionCodeForNumber(PhoneNumberUtil.getInstance().parse(fromStr, receivingPhone.getCountryCode())));
+            } catch (NumberParseException e) {
+                e.printStackTrace();
+            }
+            Date date = null;
+            try {
+                date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(msgObj.get("created_at").getAsString());
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            Message message = Message.from(sender, receivingPhone, date, msgObj.get("text").getAsString());
+            messages.add(message);
+        }
+        return messages;
     }
 
 }
