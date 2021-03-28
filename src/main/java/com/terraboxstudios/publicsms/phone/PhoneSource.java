08532a070d6f5ba384777b0dccd438badec4fae5
Ben-D-Anderson <benanderson.david@gmail.com>
2021-03-28 16:50:09 +0100

    Added documentation

--- src/main/java/com/terraboxstudios/publicsms/phone/PhoneSource.java
+++ src/main/java/com/terraboxstudios/publicsms/phone/PhoneSource.java
@@ -7,8 +7,35 @@ import java.util.Collection;
 
 public interface PhoneSource {
 
+     /**
+      * Method used to collect available PublicPhone(s) from
+      * the phone source.
+      * @return Collection of PublicPhone(s) objects which are available
+      * and provided by the phone source.
+      * @throws IOException may be thrown when communicating with a website or api.
+      */
      Collection<PublicPhone> getPhoneNumbers() throws IOException;
+
+     /**
+      * Method used to collect available PublicPhone(s) which belong to the country
+      * defined by the two-digit, uppercase ISO 3166 code the from the phone source.
+      * @param countryCode two-digit, uppercase ISO 3166 code associated with the country
+      * of the PublicPhone(s) which are being retrieved.
+      * @return Collection of PublicPhone(s) objects, which are available and who's
+      * two-digit, uppercase ISO 3166 country code matches the countryCode
+      * parameter, provided by the phone source.
+      * @throws IOException may be thrown when communicating with a website or api.
+      */
      Collection<PublicPhone> getPhoneNumbers(String countryCode) throws IOException;
+
+     /**
+      * Method used to retrieve a Collection of Message(s) sent to the PublicPhone
+      * provided by the phone source.
+      * @param receivingPhone PublicPhone to retrieve messages from.
+      * @return Collection of Message(s) objects, which are provided by the phone source
+      * and were sent to the receivingPhone.
+      * @throws IOException may be thrown when communicating with a website or api.
+      */
      Collection<Message> getMessages(PublicPhone receivingPhone) throws IOException;
 
 }
--- src/main/java/com/terraboxstudios/publicsms/phone/PublicPhoneGenerator.java
+++ src/main/java/com/terraboxstudios/publicsms/phone/PublicPhoneGenerator.java
@@ -7,6 +7,11 @@ public class PublicPhoneGenerator {
 
     private final Collection<PublicPhone> publicPhones = new HashSet<>();
 
+    /**
+     * Fills the PublicPhoneGenerator with PublicPhone(s)
+     * gathered from the PhoneSource(s).
+     * @param phoneSources PhoneSource(s) to use to get PublicPhone(s)
+     */
     public PublicPhoneGenerator(PhoneSource... phoneSources) {
         for (PhoneSource phoneSource : phoneSources) {
             try {
@@ -18,6 +23,12 @@ public class PublicPhoneGenerator {
         }
     }
 
+    /**
+     * Fills the PublicPhoneGenerator with PublicPhone(s)
+     * gathered from the PhoneSource(s) which match the specified country.
+     * @param country The country where the phone numbers should be located.
+     * @param phoneSources PhoneSource(s) to use to get PublicPhone(s)
+     */
     public PublicPhoneGenerator(Locale country, PhoneSource... phoneSources) {
         for (PhoneSource phoneSource : phoneSources) {
             try {
@@ -29,11 +40,27 @@ public class PublicPhoneGenerator {
         }
     }
 
+    /**
+     * Method to get all the PublicPhone(s) loaded into the
+     * generator from provided PhoneSource(s).
+     * @return Collection containing all PublicPhone(s). If there
+     * are no PublicPhone(s) available then the method returns
+     * an empty Collection.
+     */
     public Collection<PublicPhone> getPublicPhones() {
         return publicPhones;
     }
 
+    /**
+     * Method to get a random PublicPhone from the PublicPhone(s)
+     * loaded into the generator.
+     * @return An optional representing the randomly selected
+     * PublicPhone from those loaded into the generator.
+     * If there are no PublicPhone(s) loaded into the
+     * generator then the method will return an empty Optional.
+     */
     public Optional<PublicPhone> getRandomPublicPhone() {
+        if (publicPhones.size() == 0) return Optional.empty();
         return publicPhones.stream().skip(new Random().nextInt(publicPhones.size())).findFirst();
     }
 
