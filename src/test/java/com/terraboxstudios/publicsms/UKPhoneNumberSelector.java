c07b350d6ee1ecd0f704896bf0788e8d2a348f71
Ben-D-Anderson <benanderson.david@gmail.com>
2021-03-28 18:59:46 +0100

    Added new PhoneSource

--- src/test/java/com/terraboxstudios/publicsms/UKPhoneNumberSelector.java
+++ src/test/java/com/terraboxstudios/publicsms/UKPhoneNumberSelector.java
@@ -3,6 +3,7 @@ package com.terraboxstudios.publicsms;
 import com.terraboxstudios.publicsms.phone.PhoneSource;
 import com.terraboxstudios.publicsms.phone.PublicPhone;
 import com.terraboxstudios.publicsms.phone.PublicPhoneGenerator;
+import com.terraboxstudios.publicsms.phone.sources.OSIOPhoneSource;
 import com.terraboxstudios.publicsms.phone.sources.RFSPhoneSource;
 
 import java.util.Locale;
@@ -13,8 +14,9 @@ public class UKPhoneNumberSelector {
     public static void main(String[] args) {
 
         PhoneSource rfsPhoneSource = new RFSPhoneSource();
+        PhoneSource osioPhoneSource = new OSIOPhoneSource();
 
-        PublicPhoneGenerator publicPhoneGenerator = new PublicPhoneGenerator(Locale.UK.getCountry(), rfsPhoneSource);
+        PublicPhoneGenerator publicPhoneGenerator = new PublicPhoneGenerator(Locale.UK.getCountry(), rfsPhoneSource, osioPhoneSource);
         Optional<PublicPhone> publicPhone = publicPhoneGenerator.getRandomPublicPhone();
         publicPhone.ifPresent(phone -> System.out.println(phone.getNumber()));
 
