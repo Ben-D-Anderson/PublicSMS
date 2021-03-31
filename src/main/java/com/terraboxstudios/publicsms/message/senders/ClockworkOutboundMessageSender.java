85184a33966934033a520f51f9b9d31d430653ef
Ben-D-Anderson <benanderson.david@gmail.com>
2021-03-31 21:20:55 +0100

    Created outbound message sender to use

---
+++ src/main/java/com/terraboxstudios/publicsms/message/senders/ClockworkOutboundMessageSender.java
@@ -0,0 +1,27 @@
+package com.terraboxstudios.publicsms.message.senders;
+
+import com.clockworksms.ClockWorkSmsService;
+import com.clockworksms.ClockworkException;
+import com.clockworksms.ClockworkSmsResult;
+import com.clockworksms.SMS;
+import com.terraboxstudios.publicsms.message.OutboundMessage;
+import com.terraboxstudios.publicsms.message.OutboundMessageSender;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ClockworkOutboundMessageSender implements OutboundMessageSender<ClockworkSmsResult> {
+
+    private final String apiKey;
+
+    @Override
+    public ClockworkSmsResult send(OutboundMessage outboundMessage) throws ClockworkException {
+        ClockWorkSmsService clockWorkSmsService = new ClockWorkSmsService(apiKey);
+        SMS smsMessage = new SMS();
+        smsMessage.setTo(outboundMessage.getReceiver().getNumber());
+        smsMessage.setFrom(outboundMessage.getSender().getNumber());
+        if (outboundMessage.getBody().length() > 160) smsMessage.setLongMessage(true);
+        smsMessage.setMessage(outboundMessage.getBody());
+        return clockWorkSmsService.send(smsMessage);
+    }
+
+}
