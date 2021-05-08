91b20e8273f049c4923f17738c0d796fe1bd11cf
Ben-D-Anderson <benanderson.david@gmail.com>
2021-05-08 14:49:10 +0100

    Demonstrate receiving SMS

--- src/test/java/com/terraboxstudios/publicsms/PhoneListenerTest.java
+++ src/test/java/com/terraboxstudios/publicsms/PhoneListenerTest.java
@@ -1,17 +1,19 @@
 package com.terraboxstudios.publicsms;
 
-import com.terraboxstudios.publicsms.phone.PublicPhoneSource;
 import com.terraboxstudios.publicsms.phone.PublicPhone;
 import com.terraboxstudios.publicsms.phone.PublicPhoneGenerator;
+import com.terraboxstudios.publicsms.phone.PublicPhoneSource;
 import com.terraboxstudios.publicsms.phone.sources.OSIOPublicPhoneSource;
 import com.terraboxstudios.publicsms.phone.sources.RFSPublicPhoneSource;
+import com.terraboxstudios.publicsms.services.InboundMessageListener;
+import com.terraboxstudios.publicsms.services.InboundMessageService;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.Locale;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
-public class UKPhoneNumberSelector {
+public class PhoneListenerTest {
 
     public static void main(String[] args) {
 
@@ -22,10 +24,15 @@ public class UKPhoneNumberSelector {
         publicPhoneSources.add(rfsPublicPhoneSource);
         publicPhoneSources.add(osioPublicPhoneSource);
 
-        PublicPhoneGenerator publicPhoneGenerator = new PublicPhoneGenerator(Locale.UK.getCountry(), publicPhoneSources);
+        PublicPhoneGenerator publicPhoneGenerator = new PublicPhoneGenerator(publicPhoneSources);
         Optional<PublicPhone> publicPhone = publicPhoneGenerator.getRandomPublicPhone();
         publicPhone.ifPresent(phone -> System.out.println(phone.getNumber()));
 
+        InboundMessageListener listener = (publicPhone1, inboundMessage) -> System.out.println(inboundMessage.getSender().getNumber() + " -> " + inboundMessage.getBody());
+        Collection<InboundMessageListener> listeners = new HashSet<>();
+        listeners.add(listener);
+        InboundMessageService inboundMessageService = new InboundMessageService(1, listeners, publicPhone.get());
+        inboundMessageService.start(0, 5, TimeUnit.SECONDS);
     }
 
 }
